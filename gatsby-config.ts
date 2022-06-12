39c549582e5a1e52b6a71a71031f46a8c99d1a5d
Richard Hsu <kinghsusw@gmail.com>
2022-06-12 16:59:30 -0700

    Add default site metadata

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -4,6 +4,9 @@ import path from "path";
 const config: GatsbyConfig = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
+    title: "Richard Hsu | Software Engineer",
+    description:
+      "Software engineer portfolio website for Richard Hsu (aka Sheng-Wei Hsu).",
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
