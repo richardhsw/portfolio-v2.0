163c956afc5572b0a4d154e4c8d00b75bfc3cece
Richard Hsu <kinghsusw@gmail.com>
2022-06-12 19:06:44 -0700

    Update import name for styles
    Previously was using styles.css, but changed it to styles.module.css to match Gatsby built-in css support

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SEO from "@components/SEO";
 import { LayoutProps } from "./interface";
-import "./styles.css";
+import "./styles.module.css";
 
 function Layout({ children }: LayoutProps) {
   return (
